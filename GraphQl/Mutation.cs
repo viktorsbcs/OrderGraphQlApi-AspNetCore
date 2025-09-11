75060b2776a67971b07c08dd3b520031096177c7
Viktors Bcs <56092407+viktorsbcs@users.noreply.github.com>
2025-09-11 17:41:39 +0300

    Graphql mutations and query updates

--- GraphQl/Mutation.cs
+++ GraphQl/Mutation.cs
@@ -1,6 +1,17 @@
-﻿namespace OrderGraphQlApi.GraphQl
+﻿using Microsoft.EntityFrameworkCore;
+using OrderGraphQlApi.Context;
+using OrderGraphQlApi.Models;
+
+namespace OrderGraphQlApi.GraphQl
 {
 	public class Mutation
 	{
+		public Customer AddCustomer(string firstName, string lastName, string address, string email, GraphQlDbContext dbContext)
+		{
+			var customer = new Customer() { FirstName = firstName, LastName = lastName, Address = address, Email = email };
+			dbContext.Customers.Add(customer);
+			dbContext.SaveChanges();
+			return customer;
+		}
 	}
 }
--- GraphQl/Query.cs
+++ GraphQl/Query.cs
@@ -1,6 +1,42 @@
-﻿namespace OrderGraphQlApi.GraphQl
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using OrderGraphQlApi.Context;
+using OrderGraphQlApi.Models;
+
+namespace OrderGraphQlApi.GraphQl
 {
 	public class Query
 	{
+		[UseProjection]
+		[UseFiltering]
+		[UseSorting]
+		public IQueryable<Customer> GetCustomers(GraphQlDbContext dbContext)
+		{
+			return dbContext.Customers;
+		}
+
+		[UseProjection]
+		[UseFiltering]
+		[UseSorting]
+		public IQueryable<Product> GetProducts(GraphQlDbContext dbContext)
+		{
+			return dbContext.Products;
+		}
+
+		[UseProjection]
+		[UseFiltering]
+		[UseSorting]
+		public IQueryable<Order> GetOrders(GraphQlDbContext dbContext)
+		{
+			return dbContext.Orders;
+		}
+
+		[UseProjection]
+		[UseFiltering]
+		[UseSorting]
+		public IQueryable<OrderItem> GetOrderItems(GraphQlDbContext dbContext)
+		{
+			return dbContext.OrderItems;
+		}
 	}
 }
