73dcafea339b42c1f03a1a7658322721ff342123
Viktors Bcs <56092407+viktorsbcs@users.noreply.github.com>
2025-09-14 02:46:41 +0300

    Add graphql subscription type

---
+++ GraphQl/Subscription.cs
@@ -0,0 +1,19 @@
+﻿using OrderGraphQlApi.Models;
+
+namespace OrderGraphQlApi.GraphQl
+{
+	public class Subscription
+	{
+		[Subscribe]
+		[Topic]
+		public Customer OnCustomerAdded([EventMessage] Customer customer) => customer;
+
+		[Subscribe]
+		[Topic]
+		public Product OnProductAdded([EventMessage] Product product) => product;
+
+		[Subscribe]
+		[Topic]
+		public Order OnOrderCreated([EventMessage] Order order) => order;
+	}
+}
